adaeefe224c25a0c5f553a23209df148c05db5c1
Aida Zametica <azametica1@etf.unsa.ba>
2024-01-17 16:42:41 +0100

    adding nunit tets

---
+++ SeleniumKesten/pages/HomePage.cs
@@ -0,0 +1,101 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+
+namespace SeleniumKesten.pages
+{
+    public class HomePage
+    {
+        private IWebDriver driver;
+
+        private By searchLinkLocator = By.CssSelector("#Mod397 .item-3720 > a");
+        private By priceFromInputLocator = By.Id("price_from");
+        private By priceToInputLocator = By.Id("price_to");
+        private By searchButtonLocator = By.CssSelector(".button:nth-child(1)");
+        private By detailButtonLocator = By.CssSelector(".owl-item:nth-child(1) .button_detail:nth-child(2)");
+        private By addToWishlistButtonLocator = By.CssSelector(".btn-wishlist");
+        private By productListLocator = By.XPath("//div[@class='jshop' and descendant::div[@class='jshop_list_product']]");
+
+        public HomePage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void OpenPage(string url, int maxRetries = 3)
+        {
+            int retryCount = 0;
+            while (retryCount < maxRetries)
+            {
+                try
+                {
+                    driver.Navigate().GoToUrl(url);
+                    return;
+                }
+                catch (WebDriverException)
+                {
+                    retryCount++;
+                }
+            }
+        }
+
+        public void ClickSearchLink()
+        {
+            IWebElement searchLink = driver.FindElement(searchLinkLocator);
+            IJavaScriptExecutor executor = (IJavaScriptExecutor)driver;
+            executor.ExecuteScript("arguments[0].click();", searchLink);
+        }
+
+        public void SetPriceRange(string priceFrom, string priceTo)
+        {
+            IWebElement priceFromInput = driver.FindElement(priceFromInputLocator);
+            IWebElement priceToInput = driver.FindElement(priceToInputLocator);
+
+            priceFromInput.Clear();
+            priceFromInput.SendKeys(priceFrom);
+
+            priceToInput.Clear();
+            priceToInput.SendKeys(priceTo);
+        }
+
+        public void ClickSearchButton()
+        {
+            IWebElement searchButton = driver.FindElement(searchButtonLocator);
+            IJavaScriptExecutor executor = (IJavaScriptExecutor)driver;
+            executor.ExecuteScript("arguments[0].click();", searchButton);
+        }
+
+        public void ClickDetailButton()
+        {
+            IWebElement detailButton = driver.FindElement(detailButtonLocator);
+            IJavaScriptExecutor executor = (IJavaScriptExecutor)driver;
+            executor.ExecuteScript("arguments[0].click();", detailButton);
+        }
+
+        public void ClickAddToWishlistButton()
+        {
+            IWebElement addToWishlistButton = driver.FindElement(addToWishlistButtonLocator);
+            IJavaScriptExecutor executor = (IJavaScriptExecutor)driver;
+            executor.ExecuteScript("arguments[0].click();", addToWishlistButton);
+        }
+
+        public bool IsProductListDisplayed()
+        {
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+
+            try
+            {
+                wait.Until(ExpectedConditions.ElementIsVisible(productListLocator));
+                return true;
+            }
+            catch (WebDriverTimeoutException)
+            {
+                return false;
+            }
+        }
+
+        public void ExecuteScript(string script)
+        {
+            ((IJavaScriptExecutor)driver).ExecuteScript(script);
+        }
+    }
+}
---
+++ SeleniumKesten/tests/HomeTests.cs
@@ -0,0 +1,91 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using SeleniumKesten.pages;
+
+namespace SeleniumKesten.tests
+{
+    public class HomeTests
+    {
+        private IWebDriver driver;
+        private HomePage homePage;
+
+        [SetUp]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            homePage = new HomePage(driver);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+
+        [Test]
+        [TestCase("200", "200")]
+        [TestCase("1000", "100")]
+        public void TestValidanRangeCijena(string maxPrice, string minPrice)
+        {
+            homePage.OpenPage("https://kesten.ba/index.php/bih/");
+            homePage.ClickSearchLink();
+            homePage.SetPriceRange(minPrice, maxPrice);
+            homePage.ClickSearchButton();
+
+            Assert.IsTrue(homePage.IsProductListDisplayed(), "Lista produkata treba da bude prikazana.");
+
+        }
+
+
+        [Test]
+        [TestCase("0", "0")] // BUG - prikazuje sve artikle web shopa uprkos nultim vrijednostima minimalne i maksimalne cijene
+        [TestCase("-100", "-50")] 
+        [TestCase("-50", "-100")]
+        [TestCase("50", "100")]
+        [TestCase("-50", "-50")]
+        [TestCase("100", "-100")] // BUG - prikazuje proizvode cije su cijene u rangu 0 - 100KM, uprkos negativnoj minimalnoj cijeni
+        [TestCase("-100", "100")]
+        public void TestNevalidanRangeCijena(string maxPrice, string minPrice)
+        {
+            homePage.OpenPage("https://kesten.ba/index.php/bih/");
+            homePage.ClickSearchLink();
+            homePage.SetPriceRange(minPrice, maxPrice);
+            homePage.ClickSearchButton();
+
+            Assert.IsFalse(homePage.IsProductListDisplayed(), "Lista produkata ne treba biti prikazana, ocekuje se not found result.");
+
+        }
+
+
+        // BUG - dodavanje proizvoda na wishlistu otvara cart pogled
+        [Test]
+        public void DodavanjeProizvoljnogProizvodaUWishlistu()
+        {
+            homePage.OpenPage("https://kesten.ba/index.php/bih/");
+            homePage.ClickDetailButton();
+            homePage.ClickAddToWishlistButton();
+
+            // Ovaj assert je napravljen kako bi test pao, kad zelimo da assignamo bug nekome, simulira AzureDevOps workflow    
+            Assert.DoesNotThrow(() =>
+            {
+                homePage.ExecuteScript("if (window.location.href.includes('https://kesten.ba/index.php/bih/shopping-korpa/cart/add')) { throw new Error('URL match error'); }");
+            }, "URL match error");
+
+
+            /*
+
+            // Koristimo ovaj assert kad zelimo da test prodje
+            var exception = Assert.Throws<JavaScriptException>(() =>
+            {
+                homePage.ExecuteScript("if (window.location.href.includes('https://kesten.ba/index.php/bih/shopping-korpa/cart/add')) { throw new Error('URL match error'); }");
+            });
+
+            */
+
+
+        }
+
+
+    }
+}
